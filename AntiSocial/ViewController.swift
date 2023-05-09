96e3def21f39edd8d5849dd725aaab891f9c46b1
Togosling <toga.abdyldaev@gmail.com>
2023-05-09 22:46:00 +0800

    Initial Commit

---
+++ AntiSocial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AntiSocial
+//
+//  Created by Тагай Абдылдаев on 2023/5/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
